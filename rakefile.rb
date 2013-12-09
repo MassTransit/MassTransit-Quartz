59473e7f0172fa8e5b28d1f8954c0ae2b0191683
Chris Patterson <chris@phatboyg.com>
2013-12-08 20:57:02 -0800

    Updated version

--- rakefile.rb
+++ rakefile.rb
@@ -149,7 +149,7 @@ nuspec :create_nuspec do |nuspec|
   nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
   nuspec.requireLicenseAcceptance = "false"
   nuspec.dependency "Magnum", "2.1.1"
-  nuspec.dependency "MassTransit", "2.9.1"
+  nuspec.dependency "MassTransit", "2.9.2"
   nuspec.output_file = File.join(props[:artifacts], 'MassTransit.Scheduling.nuspec')
   add_files File.join(props[:output], 'Scheduling'), 'MassTransit.Scheduling.{dll,pdb,xml}', nuspec
   nuspec.file(File.join(props[:src], "MassTransit.Scheduling\\**\\*.cs").gsub("/","\\"), "src")
@@ -168,7 +168,7 @@ nuspec :create_nuspec do |nuspec|
   nuspec.licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0"
   nuspec.requireLicenseAcceptance = "false"
   nuspec.dependency "Magnum", "2.1.1"
-  nuspec.dependency "MassTransit", "2.9.1"
+  nuspec.dependency "MassTransit", "2.9.2"
   nuspec.dependency "MassTransit.Scheduling", NUGET_VERSION
   nuspec.dependency "Common.Logging", "2.1.2"
   nuspec.dependency "Newtonsoft.Json", "5.0.8"
